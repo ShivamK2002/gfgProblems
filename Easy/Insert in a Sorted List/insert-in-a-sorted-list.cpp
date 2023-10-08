e64fa6dc030612180f93772ecd5c78c4739b4d75
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2023-10-08 18:42:38 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Insert in a Sorted List/insert-in-a-sorted-list.cpp
@@ -0,0 +1,111 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+}
+
+// } Driver Code Ends
+/*
+structure of the node of the list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution{
+  public:
+    // Should return head of the modified linked list
+    Node *sortedInsert(struct Node* head, int data) {
+        // Code here
+        Node* prev = NULL;
+        Node* temp = head;
+        Node* newNode = new Node(data);
+        while(temp!=NULL){
+            if(temp->data<=data) {
+                prev = temp;
+                temp= temp->next;
+            }
+            else break;
+        }
+        if(temp==NULL){
+            prev->next = newNode;
+            return head;
+        }
+        if(temp->data>data){
+             if(prev==NULL){
+                 newNode->next = head;
+                 return newNode;
+             }
+             
+             else{
+                 prev->next = newNode;
+                 newNode->next = temp;
+                 return head;
+             }
+             
+        }
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+        
+		int data;
+		cin>>data;
+		
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		
+		int k;
+		cin>>k;
+		Solution obj;
+		head = obj.sortedInsert(head,k);
+		printList(head); 
+	}
+	return 0; 
+} 
+
+// } Driver Code Ends
