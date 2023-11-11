f71d82a0de43d6ffcf1f3eac6e1c77c38c0f5554
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2023-11-11 20:25:14 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Isomorphic Strings/isomorphic-strings.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// C++ program to check if two strings are isomorphic
+
+
+#include<bits/stdc++.h>
+
+
+using namespace std;
+#define MAX_CHARS 256
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to check if two strings are isomorphic.
+    bool areIsomorphic(string str1, string str2)
+    {
+        
+        // Your code here
+        if(str1.length()!=str2.length()) return false;
+        int hash[256] = {0};
+        bool mapped[256] = {0};
+        for(int i=0; i<str1.size() ;i++){
+            if(hash[str1[i]]==0 && mapped[str2[i]]==0)
+            hash[str1[i]]=str2[i];
+            
+            mapped[str2[i]]=true;
+        }
+        for(int i=0; i<str1.size();i++){
+            if(char(hash[str1[i]])!=str2[i]) return false;
+        }
+        return true;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver program
+int main()
+{
+    int t;
+    cin>>t;
+    string s1,s2;
+    while (t--) {
+        cin>>s1;
+        cin>>s2;
+        Solution obj;
+        cout<<obj.areIsomorphic(s1,s2)<<endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
